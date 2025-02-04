075592be6794f20ca6870c0ee7ef0aad60a5eaee
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-04 11:21:38 +0000

    Added findByMonth query.

--- src/main/java/com/example/SoloProject/PlantsRepository.java
+++ src/main/java/com/example/SoloProject/PlantsRepository.java
@@ -10,6 +10,6 @@ public interface PlantsRepository extends CrudRepository<Plants, Integer> {
     @Query("SELECT p FROM Plants p JOIN FETCH p.jarPrice WHERE p.id = (:id)")
     public List<Plants> findJarPriceById(int id);
 
-
+    @Query("SELECT p FROM Plants p WHERE p.month = (:month)")
     public List<Plants> findByMonth(int month);
 }
