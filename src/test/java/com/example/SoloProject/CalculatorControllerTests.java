75413abda2e16cdc89f42b1c6325e7d8814e2461
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-04 16:36:09 +0000

    Created controller tests and status code tests.

---
+++ src/test/java/com/example/SoloProject/CalculatorControllerTests.java
@@ -0,0 +1,19 @@
+package com.example.SoloProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CalculatorControllerTests {
+
+    @Autowired
+    private CalculatorController calculatorController;
+
+    @Test
+    public void contextLoads(){
+        assertNotNull(calculatorController);
+    }
+}
--- src/test/java/com/example/SoloProject/SoloProjectApplicationTests.java
+++ src/test/java/com/example/SoloProject/SoloProjectApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.SoloProject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SoloProjectApplicationTests {
+
+	@Autowired
+	MockMvc mvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void homePageReturnsOk() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void dataPageReturnsOk() throws Exception {
+		mvc.perform(get("/crops")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void calculatorPageReturnsOk() throws Exception {
+		mvc.perform(get("/calculator")).andExpect(status().isOk());
 	}
 
 
