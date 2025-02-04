9fd670ff39f6735e4d46330475abb1db2823d473
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-04 11:21:17 +0000

    Created plant controller.

--- src/main/java/com/example/SoloProject/PlantsController.java
+++ src/main/java/com/example/SoloProject/PlantsController.java
@@ -1,4 +1,23 @@
 package com.example.SoloProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
 public class PlantsController {
+
+    @Autowired
+    PlantsRepository plantsRepository;
+
+    @RequestMapping("/crops")
+    public String viewCropDatabasePage(Model model){
+        List<Plants> plantsList = (List<Plants>) plantsRepository.findAll();
+        model.addAttribute("listPlants", plantsList);
+        return "dataPage";
+    }
+
 }
