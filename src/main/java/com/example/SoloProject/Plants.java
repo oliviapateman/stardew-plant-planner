de634dbb876d62148e2dbb6d919076e008e0f2d6
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-04 10:14:45 +0000

    Added table name and populated database.

--- src/main/java/com/example/SoloProject/Plants.java
+++ src/main/java/com/example/SoloProject/Plants.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "Plants")
 public class Plants {
 
     @Id
--- src/main/java/com/example/SoloProject/SoloProjectApplication.java
+++ src/main/java/com/example/SoloProject/SoloProjectApplication.java
@@ -37,14 +37,35 @@ public class SoloProjectApplication {
 
 			//Summer
 			plantsRepo.save(new Plants(true, false, "Blueberry", 2, List.of(50, 62, 75)));
+			plantsRepo.save(new Plants(false, false, "Corn", 2, List.of(50, 62, 75)));
 			plantsRepo.save(new Plants(false, true, "Hops", 2, List.of(25, 31, 37)));
-
-
+			plantsRepo.save(new Plants(true, false, "Hot Pepper", 2, List.of(40, 50, 60)));
+			plantsRepo.save(new Plants(true, false, "Melon", 2, List.of(250, 312, 375)));
+			plantsRepo.save(new Plants(false, true, "Poppy", 2, List.of(140, 175, 210)));
+			plantsRepo.save(new Plants(false, false, "Radish", 2, List.of(90, 112, 135)));
+			plantsRepo.save(new Plants(false, true, "Summer Spangle", 2, List.of(90, 112, 135)));
+			plantsRepo.save(new Plants(true, false, "Summer Squash", 2, List.of(45, 56, 67)));
+			plantsRepo.save(new Plants(false, true, "Sunflower", 2, List.of(80, 100, 120)));
+			plantsRepo.save(new Plants(false, false, "Tomato", 2, List.of(60,75,90)));
+			plantsRepo.save(new Plants(false, false, "Wheat", 2, List.of(25, 31, 37)));
 
 			//Fall
-
+			plantsRepo.save(new Plants(false, false, "Corn", 3, List.of(50, 62, 75)));
+			plantsRepo.save(new Plants(false, true, "Sunflower", 3, List.of(80, 100, 120)));
+			plantsRepo.save(new Plants(false, false, "Wheat", 3, List.of(25, 31, 37)));
+			plantsRepo.save(new Plants(false, false, "Amaranth", 3, List.of(150, 187, 225)));
+			plantsRepo.save(new Plants(false, false, "Artichoke", 3, List.of(160, 200, 240)));
+			plantsRepo.save(new Plants(false, false, "Bok Choy", 3, List.of(80, 100, 120)));
+			plantsRepo.save(new Plants(false, false, "Broccoli", 3, List.of(70, 7, 105)));
+			plantsRepo.save(new Plants(true, false, "Cranberries", 3, List.of(75, 93, 112)));
+			plantsRepo.save(new Plants(false, false, "Eggplant", 3, List.of(60, 75, 90)));
+			plantsRepo.save(new Plants(false, true, "Fairy Rose", 3, List.of(290, 362, 435)));
+			plantsRepo.save(new Plants(true, false, "Grape", 3, List.of(80, 100, 120)));
+			plantsRepo.save(new Plants(false, false, "Pumpkin", 3, List.of(320, 400, 480)));
+			plantsRepo.save(new Plants(false, false, "Yam", 3, List.of(160, 200, 240)));
 
 			//Winter
+			plantsRepo.save(new Plants(true, false, "Powdermelon", 4, List.of(60, 75, 90)));
 
 
 			plantsRepo.findAll().forEach(plants -> log.info(plants.toString()));
