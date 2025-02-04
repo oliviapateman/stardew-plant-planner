0d55083d2918038fda2d73b80f6bb8b0f9e3d252
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-04 16:31:04 +0000

    Created Calculator class and controller.

--- src/main/java/com/example/SoloProject/Calculator.java
+++ src/main/java/com/example/SoloProject/Calculator.java
@@ -1,4 +1,60 @@
 package com.example.SoloProject;
 
 public class Calculator {
+
+    private int quantity;
+    private boolean isTiller;
+    private boolean isKeg;
+    private boolean isKegArtisan;
+    private boolean isJar;
+    private boolean isJarArtisan;
+
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isTiller() {
+        return isTiller;
+    }
+
+    public void setTiller(boolean tiller) {
+        isTiller = tiller;
+    }
+
+    public boolean isKeg() {
+        return isKeg;
+    }
+
+    public void setKeg(boolean keg) {
+        isKeg = keg;
+    }
+
+    public boolean isKegArtisan() {
+        return isKegArtisan;
+    }
+
+    public void setKegArtisan(boolean kegArtisan) {
+        isKegArtisan = kegArtisan;
+    }
+
+    public boolean isJar() {
+        return isJar;
+    }
+
+    public void setJar(boolean jar) {
+        isJar = jar;
+    }
+
+    public boolean isJarArtisan() {
+        return isJarArtisan;
+    }
+
+    public void setJarArtisan(boolean jarArtisan) {
+        isJarArtisan = jarArtisan;
+    }
 }
--- src/main/java/com/example/SoloProject/CalculatorController.java
+++ src/main/java/com/example/SoloProject/CalculatorController.java
@@ -1,4 +1,36 @@
 package com.example.SoloProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
 public class CalculatorController {
+
+    @Autowired
+    PlantsRepository plantsRepo;
+
+    /*@RequestMapping("/calculator")
+    public String getProfitTotal(@Param("quantities") List<Integer> quantities, Model model){
+        model.addAttribute("quantities", quantities);
+        List<Plants> plantsList = (List<Plants>) plantsRepo.findAll();
+        model.addAttribute("listPlants", plantsList);
+        *//*TODO: calculation method
+          - quantity * price *//*
+        // probability crops --> 47% base, 33% silver, 20% gold
+        // jar / wine --> base unless aged to silver and aged to gold
+        return "calculatorPage";
+    }*/
+
+    @RequestMapping("/calculator")
+    public String viewCalcPage(Model model){
+        List<Plants> plantsList = (List<Plants>) plantsRepo.findAll();
+        model.addAttribute("listPlants", plantsList);
+        return "calculatorPage";
+    }
+
 }
