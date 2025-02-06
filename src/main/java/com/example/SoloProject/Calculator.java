7c2bf9e64f5fca447f718896fb1c5127ba556859
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-06 12:01:02 +0000

    Added calculation methods.

--- src/main/java/com/example/SoloProject/Calculator.java
+++ src/main/java/com/example/SoloProject/Calculator.java
@@ -1,60 +1,115 @@
 package com.example.SoloProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class Calculator {
 
-    private int quantity;
-    private boolean isTiller;
-    private boolean isKeg;
-    private boolean isKegArtisan;
-    private boolean isJar;
-    private boolean isJarArtisan;
+    @Autowired
+    PlantsRepository plantsRepo;
 
+    int quantity;
+    double regular = quantity / 0.47;
+    double silver = quantity / 0.33;
+    double gold = quantity / 0.2;
 
-    public int getQuantity() {
-        return quantity;
+    List<Integer> allRegArtisan = new ArrayList<>();
+    int totalRegArtisan;
+    List<Integer> allSilverArtisan = new ArrayList<>();
+    int totalSilverArtisan;
+    List<Integer> allGoldArtisan = new ArrayList<>();
+    int totalGoldArtisan;
+    List<Integer> allRegJarAndKeg = new ArrayList<>();
+    int totalRegJarKeg;
+    List<Integer> allSilverJarAndKeg = new ArrayList<>();
+    int totalSilverJarKeg;
+    List<Integer> allGoldJarAndKeg = new ArrayList<>();
+    int totalGoldJarKeg;
+    List<Integer> allNormalAndTillerCrops = new ArrayList<>();
+    int totalCrops;
+
+    public void calculateProfits() {
+        for (Plants plant : plantsRepo.findAll()) {
+            quantity = plant.getQuantity();
+            if (plant.isArtisan()) {
+                if (plant.isJar()){
+                    allRegArtisan.add(plant.getJarArtisanPrice().get(0) * quantity);
+                    allSilverArtisan.add(plant.getJarArtisanPrice().get(1) * quantity);
+                    allGoldArtisan.add(plant.getJarArtisanPrice().get(2) * quantity);
+                } else if (plant.isKeg()){
+                    allRegArtisan.add(plant.getKegArtisanPrice().get(0) * quantity);
+                    allSilverArtisan.add(plant.getKegArtisanPrice().get(1) * quantity);
+                    allGoldArtisan.add(plant.getKegArtisanPrice().get(2) * quantity);
+                }
+            } else if (plant.isJar()) {
+                allRegJarAndKeg.add((int) (plant.getJarPrice().get(0) * regular));
+                allSilverJarAndKeg.add((int) (plant.getJarPrice().get(1) * silver));
+                allGoldJarAndKeg.add((int) (plant.getJarPrice().get(2) * gold));
+            } else if (plant.isKeg()) {
+                allRegJarAndKeg.add((int) (plant.getKegPrice().get(0) * regular));
+                allSilverJarAndKeg.add((int) (plant.getKegPrice().get(1) * silver));
+                allGoldJarAndKeg.add((int) (plant.getKegPrice().get(2) * gold));
+            } else if (plant.isTiller()) {
+                allNormalAndTillerCrops.add((int) (plant.getTillerPrice().get(0) * regular));
+                allNormalAndTillerCrops.add((int) (plant.getTillerPrice().get(1) * silver));
+                allNormalAndTillerCrops.add((int) (plant.getTillerPrice().get(2) * gold));
+            } else {
+                allNormalAndTillerCrops.add((int) (plant.getNormalPrice().get(0) * regular));
+                allNormalAndTillerCrops.add((int) (plant.getNormalPrice().get(1) * silver));
+                allNormalAndTillerCrops.add((int) (plant.getNormalPrice().get(2) * gold));
+            }
+        }
+        addAllUp();
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void addAllUp() {
+        totalRegArtisan = sumOfList(allRegArtisan);
+        totalSilverArtisan = sumOfList(allSilverArtisan);
+        totalGoldArtisan = sumOfList(allGoldArtisan);
+        totalRegJarKeg = sumOfList(allRegJarAndKeg);
+        totalSilverJarKeg = sumOfList(allSilverJarAndKeg);
+        totalGoldJarKeg = sumOfList(allGoldJarAndKeg);
+        totalCrops = sumOfList(allNormalAndTillerCrops);
     }
 
-    public boolean isTiller() {
-        return isTiller;
+    public int sumOfList(List<Integer> list) {
+        int total = 0;
+        for (int item : list) {
+            total += item;
+        }
+        return total;
     }
 
-    public void setTiller(boolean tiller) {
-        isTiller = tiller;
+    public int getTotalRegArtisan() {
+        return totalRegArtisan;
     }
 
-    public boolean isKeg() {
-        return isKeg;
+    public int getTotalSilverArtisan() {
+        return totalSilverArtisan;
     }
 
-    public void setKeg(boolean keg) {
-        isKeg = keg;
+    public int getTotalGoldArtisan() {
+        return totalGoldArtisan;
     }
 
-    public boolean isKegArtisan() {
-        return isKegArtisan;
+    public int getTotalRegJarKeg() {
+        return totalRegJarKeg;
     }
 
-    public void setKegArtisan(boolean kegArtisan) {
-        isKegArtisan = kegArtisan;
+    public int getTotalSilverJarKeg() {
+        return totalSilverJarKeg;
     }
 
-    public boolean isJar() {
-        return isJar;
+    public int getTotalGoldJarKeg() {
+        return totalGoldJarKeg;
     }
 
-    public void setJar(boolean jar) {
-        isJar = jar;
+    public int getTotalCrops() {
+        return totalCrops;
     }
 
-    public boolean isJarArtisan() {
-        return isJarArtisan;
-    }
-
-    public void setJarArtisan(boolean jarArtisan) {
-        isJarArtisan = jarArtisan;
-    }
 }
