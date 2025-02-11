a25b6ab3676131fbb9f39bb65119b76957df49c8
Olivia Pateman <olivia.pateman@ten10.com>
2025-02-11 10:03:13 +0000

    Added plant repository model to controller.

--- src/main/java/com/example/SoloProject/CalendarController.java
+++ src/main/java/com/example/SoloProject/CalendarController.java
@@ -1,13 +1,22 @@
 package com.example.SoloProject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class CalendarController {
 
+    @Autowired
+    PlantsRepository plantsRepo;
+
     @RequestMapping("/calendar")
-    public String viewCalendarPage(){
+    public String viewCalendarPage(Model model){
+        List<Plants> plantsList = (List<Plants>) plantsRepo.findAll();
+        model.addAttribute("listPlants", plantsList);
         return "calendarPage";
     }
 
